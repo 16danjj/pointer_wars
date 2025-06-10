64faa07a9aebb93ca7f9c9db217becf5d1099e20
Dan Jose <16danjj@gmail.com>
2025-06-10 19:51:57 +0200

    Bug fixes, code comments, licensing information and some code changes

--- week1/linked_list.c
+++ week1/linked_list.c
@@ -1,5 +1,28 @@
+/*
+
+MIT License
+
+Copyright (c) 2025 Dan Jose
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. */
+
 #include "linked_list.h"
-#include <stdlib.h>
 
 // Function pointers to (potentially) custom malloc() and
 // free() functions.
@@ -7,28 +30,32 @@
 static void * (*malloc_fptr)(size_t size) = NULL;
 static void   (*free_fptr)(void* addr)    = NULL; 
 
-
+// Registers malloc function pointer to use function defined in linked_list_test_program.c 
 bool linked_list_register_malloc(void * (*malloc)(size_t)) {
     malloc_fptr = malloc;
     return true;
 }
 
+// Registers free function pointer to use function defined in linked_list_test_program.c 
 bool linked_list_register_free(void (*free)(void*)) {
     free_fptr = free;
     return true;
 } 
 
+// Creates a new linked list
 struct linked_list * linked_list_create(void) {
 
     struct linked_list *ll = (struct linked_list *)malloc_fptr(sizeof(struct linked_list));
 
     if (ll != NULL) {
         ll->head = NULL;
+        ll->size = 0;
     }
 
     return ll;
 }
 
+// Deletes a linked list, also makes sure all nodes are properly freed before list deletion
 bool linked_list_delete(struct linked_list * ll) { 
     
     if (ll == NULL) {
@@ -42,34 +69,28 @@ bool linked_list_delete(struct linked_list * ll) {
         while (current_node != NULL) {
             struct node *next_node = current_node->next;
             free_fptr(current_node);
+            ll->size -= 1;
 
             current_node = next_node;
         }
     }
 
+    free_fptr(ll);
     return true;
 
 }
 
+// Returns current size of the linked list
 size_t linked_list_size(struct linked_list * ll) {
-
-    size_t count = 0;
 
     if (ll == NULL) {
         return SIZE_MAX;
     }
 
-    struct node *current_node = ll->head;
-
-    while (current_node != NULL) {
-        count += 1; 
-        current_node = current_node->next;
-    }
-
-    return count;
+    return ll->size;
 }
 
-
+// Creates a new node to insert, and inserts at end of linked list
 bool linked_list_insert_end(struct linked_list * ll, unsigned int data) {
 
     struct node *node_to_insert = (struct node *)malloc_fptr(sizeof(struct node));
@@ -93,10 +114,12 @@ bool linked_list_insert_end(struct linked_list * ll, unsigned int data) {
     }
 
     current_node->next = node_to_insert;
+    ll->size += 1;
 
     return true;
 }
 
+// Creates a new node to insert, and inserts at front of linked list
 bool linked_list_insert_front(struct linked_list * ll, unsigned int data) {
 
     struct node *node_to_insert = (struct node *)malloc_fptr(sizeof(struct node));
@@ -116,10 +139,12 @@ bool linked_list_insert_front(struct linked_list * ll, unsigned int data) {
         ll->head = node_to_insert;
     }
 
+    ll->size += 1;
     return true;
 
 }
 
+// Creates a new node to insert at a particular index in linked list
 bool linked_list_insert(struct linked_list * ll, size_t index, unsigned int data) {
 
     if (index == 0) {
@@ -147,9 +172,11 @@ bool linked_list_insert(struct linked_list * ll, size_t index, unsigned int data
     prev_node->next = node_to_insert;
     node_to_insert->next = next_node;
 
+    ll->size += 1;
     return true;
 }
 
+// Returns index of matching node based on data field
 size_t linked_list_find(struct linked_list * ll, unsigned int data) {
 
     if (ll == NULL) {
@@ -176,6 +203,7 @@ size_t linked_list_find(struct linked_list * ll, unsigned int data) {
     return SIZE_MAX;
 }
 
+// Removes node specified at index from linked list
 bool linked_list_remove(struct linked_list * ll, size_t index) {
 
     if (ll == NULL) {
@@ -204,10 +232,12 @@ bool linked_list_remove(struct linked_list * ll, size_t index) {
 
     prev_node->next = current_node->next;
     free_fptr(current_node);
+    ll->size -= 1;
 
     return true;
 }
 
+// Creates an iterator over the linked list
 struct iterator * linked_list_create_iterator(struct linked_list * ll, size_t index) {
 
     struct iterator *it = (struct iterator *)malloc_fptr(sizeof(struct iterator));
@@ -232,7 +262,7 @@ struct iterator * linked_list_create_iterator(struct linked_list * ll, size_t in
     return it;
 }
 
-
+// Deletes iterator over the linked list
 bool linked_list_delete_iterator(struct iterator * iter) {
 
     if (iter == NULL) {
@@ -243,7 +273,7 @@ bool linked_list_delete_iterator(struct iterator * iter) {
     return true;
 }
 
-
+// Iterator stores node information of next node in linked list
 bool linked_list_iterate(struct iterator * iter) {
     if (iter == NULL || iter->current_node == NULL) {
         return false; 
